0d919c224a6f1605c32151aecd3ad2b6ed367409
Łukasz Leszko <leszko.lucas@gmail.com>
2025-01-23 00:30:28 +0100

    Update packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,10 @@ import { remarkReadingTime } from "./src/utils/remarkReadingTime.ts"
 import remarkUnwrapImages from "remark-unwrap-images"
 import rehypeExternalLinks from "rehype-external-links"
 import expressiveCode from "astro-expressive-code"
+import webmanifest from "astro-webmanifest";
 import { expressiveCodeOptions } from "./src/site.config"
 import icon from "astro-icon"
+import { siteConfig } from "./src/site.config"
 
 // import vercel from "@astrojs/vercel"
 
@@ -21,7 +23,40 @@ export default defineConfig({
 		}),
 		sitemap(),
 		mdx(),
-		icon()
+		icon(),
+		webmanifest({
+			// See: https://github.com/alextim/astro-lib/blob/main/packages/astro-webmanifest/README.md
+			name: siteConfig.title,
+			description: siteConfig.description,
+			lang: siteConfig.lang,
+			icon: "public/favicon/favicon.svg",
+			icons: [
+				{
+					src: "favicon/apple-touch-icon.png",
+					sizes: "180x180",
+					type: "image/png",
+				},
+				{
+					src: "favicon/web-app-manifest-192x192.png",
+					sizes: "192x192",
+					type: "image/png",
+				},
+				{
+					src: "favicon/web-app-manifest-512x512.png",
+					sizes: "512x512",
+					type: "image/png",
+				},
+			],
+			start_url: "/",
+			background_color: "#1d1f21",
+			theme_color: "#2bbc8a",
+			display: "standalone",
+			config: {
+				insertFaviconLinks: false,
+				insertThemeColorMeta: false,
+				insertManifestLink: false,
+			},
+		}),
 	],
 	markdown: {
 		remarkPlugins: [remarkUnwrapImages, remarkReadingTime],
